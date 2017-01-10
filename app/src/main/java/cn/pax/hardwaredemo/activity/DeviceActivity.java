592e7101e71d7dc31d3da8361eb192d4825585f5
chen511797015 <chen511797015@126.com>
2017-01-10 09:54:43 +0800

    更新设备信息界面

--- app/src/main/java/cn/pax/hardwaredemo/activity/DeviceActivity.java
+++ app/src/main/java/cn/pax/hardwaredemo/activity/DeviceActivity.java
@@ -1,15 +1,52 @@
 package cn.pax.hardwaredemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.pax.hardwaredemo.R;
+import cn.pax.hardwaredemo.base.BaseActivity;
 
-public class DeviceActivity extends AppCompatActivity {
+
+/**
+ * 本机信息
+ */
+
+public class DeviceActivity extends BaseActivity {
+
+    private static final String TAG = "DeviceActivity";
+
+    @BindView(R.id.iv_device_back)
+    ImageView ivDeviceBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device);
+        super.onCreate(savedInstanceState, R.layout.activity_device);
+
+    }
+
+    @Override
+    protected void findView() {
+        ButterKnife.bind(this);//绑定activity
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+        ivDeviceBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    @Override
+    protected void init() {
+
     }
 }
