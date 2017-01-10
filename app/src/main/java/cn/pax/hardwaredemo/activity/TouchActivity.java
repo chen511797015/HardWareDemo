619eb4e37962998e6a706239fa4f312fb7ceeb0f
chen511797015 <chen511797015@126.com>
2017-01-10 11:42:49 +0800

    更新Touch功能

--- app/src/main/java/cn/pax/hardwaredemo/activity/TouchActivity.java
+++ app/src/main/java/cn/pax/hardwaredemo/activity/TouchActivity.java
@@ -1,15 +1,68 @@
 package cn.pax.hardwaredemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import cn.pax.hardwaredemo.R;
+import cn.pax.hardwaredemo.base.BaseActivity;
+import cn.pax.hardwaredemo.ui.MultiTouchView;
 
-public class TouchActivity extends AppCompatActivity {
+
+/**
+ * 触摸测试
+ */
+
+public class TouchActivity extends BaseActivity {
+    private static final String TAG = "TouchActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_touch);
+
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 设置成全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState, R.layout.activity_touch);
+
+        // 设置为上面的MTView
+        setContentView(new MultiTouchView(this));
+
+    }
+
+    @Override
+    protected void findView() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void init() {
+
+        //startApk();
+
+
+    }
+
+    private void startApk() {
+
+        try {
+            ComponentName componetName = new ComponentName("com.itxinke.multitouch", "com.itxinke.multitouch.Multitouch");
+            Intent intent = new Intent();
+            intent.setComponent(componetName);
+            startActivity(intent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "没有该app存在");
+        }
     }
 }
