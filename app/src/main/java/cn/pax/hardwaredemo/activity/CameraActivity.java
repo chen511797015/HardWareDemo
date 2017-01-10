447f42c981584474f9694a386527dbaa12bf6738
chen511797015 <chen511797015@126.com>
2017-01-10 10:45:29 +0800

    更新Camera布局界面

--- app/src/main/java/cn/pax/hardwaredemo/activity/CameraActivity.java
+++ app/src/main/java/cn/pax/hardwaredemo/activity/CameraActivity.java
@@ -1,15 +1,58 @@
 package cn.pax.hardwaredemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.pax.hardwaredemo.R;
+import cn.pax.hardwaredemo.base.BaseActivity;
 
-public class CameraActivity extends AppCompatActivity {
+
+/**
+ * 摄像头界面
+ */
+
+public class CameraActivity extends BaseActivity {
+
+    private static final String TAG = "CameraActivity";
+
+    ImageView iv_camera_back;//返回
+    @BindView(R.id.iv_camera_back)
+    ImageView ivCameraBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        Log.d(TAG, "初始化Camera界面...");
+        super.onCreate(savedInstanceState, R.layout.activity_camera);
+
+    }
+
+    @Override
+    protected void findView() {
+
+        //绑定ButterKnife
+        ButterKnife.bind(this);
+
+    }
+
+
+    @OnClick(R.id.iv_camera_back)
+    public void back() {
+        Log.d(TAG, "back: ");
+        onBackPressed();
+    }
+
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void init() {
+
     }
 }
